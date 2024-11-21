866659c96358fd178ad5379fd6c59c4fb6eb13b9
Joe Diden <2106488420@qq.com>
2024-11-21 16:27:35 +1100

    handling error

--- src/pages/catfact.tsx
+++ src/pages/catfact.tsx
@@ -3,12 +3,27 @@
 
 // This gets called on every request
 export async function getServerSideProps() {
+  try {
     // Fetch data from external API
     const res = await fetch(`https://api.ipify.org/?format=json`)
-    const data = await res.json()
+    if (!res.ok) {
+      console.error("This is no good at all");
+      throw new Error('error happened in fetching from backend')
+    }
+    const data = { des: "void" }
     // Pass data to the page via props
     return { props: { data } }
+  } catch (error) {
+    console.error("Error occured while fetching IP address:" + error)
+    const data = { error: "Error fetching from IP address" }
+    console.log({
+      props: { data }
+    })
+    return {
+      props: { data }
+    }
   }
+}
 
 
 
--- src/pages/clientsidenameguessing.tsx
+++ src/pages/clientsidenameguessing.tsx
@@ -3,10 +3,18 @@ export default function CatFactDetail() {
     const [name, setName] = useState('');
     const [data, setData] = useState(null);
     const handleSubmit = async (e) => {
-        e.preventDefault(); // Fetch data from the external API with the name parameter 
-        const res = await fetch(`https://api.genderize.io/?name=${name}`);
-        const result = await res.json(); // Update the data state with the fetched result 
-        setData(result);
+        try {
+            e.preventDefault(); // Fetch data from the external API with the name parameter 
+            const res = await fetch(`https://api.genderize.io/?name=${name}`);
+            const result = await res.json(); // Update the data state with the fetched result 
+            if(res.ok) {
+                throw new DOMException("There is a connectivity Issue");
+            }
+            setData(result);
+        } catch (error) {
+            console.error("error occured while fetching from API", error);
+            setData({res: "Http fetch error"});
+        }
     };
     return (<div> <form onSubmit={handleSubmit}> <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter name" /> <button type="submit">Search</button>
     </form> {data && <h1>{JSON.stringify(data)}</h1>}
